d7d5cf015f7c8d7b7ac877f0337752861627f9fe
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-01-20 13:38:42 -0300

    function theme changing ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+const htmlElement = document.getElementById('htmlElement')
 
+const theme = htmlElement.getAttribute("data-bs-theme")
 
-const toggle = () => {
-
-}
+const toggle = () => htmlElement.setAttribute('data-bs-theme', htmlElement.getAttribute('data-bs-theme') === 'dark' ? 'light' : 'dark')
